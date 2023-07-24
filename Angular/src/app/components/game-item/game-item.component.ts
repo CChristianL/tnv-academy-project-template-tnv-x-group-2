5f5b6e43c3f52d6c5350c35def98d45746ea2ac3
Christian Luigi Cappai <chriscrow@outlook.it>
2023-07-24 02:24:13 +0200

    CC 24 luglio - Ripassare Event Emitter comment

--- Angular/src/app/components/game-item/game-item.component.ts
+++ Angular/src/app/components/game-item/game-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
+import { Rating } from 'src/app/models/rating';
 
 @Component({
   selector: 'tnv-game-item',
@@ -7,7 +8,9 @@ import { Movie } from 'src/app/models/movie';
   styleUrls: ['./game-item.component.scss']
 })
 export class GameItemComponent {
+[x: string]: any;
    @Input() movie: Movie | undefined;
-   @Input() movies: Movie [] | undefined;
+   @Input() movies: Movie [] = [];
+   @Input() rating: Rating | undefined;
 }
 
