b4c2849b5c7e9e2dc1209551cb59356ace5edf08
Christian Luigi Cappai <chriscrow@outlook.it>
2023-07-31 22:59:16 +0200

    filmIta

--- Angular/src/app/services/movie.service.ts
+++ Angular/src/app/services/movie.service.ts
@@ -10,7 +10,7 @@ export class MovieService {
 
   movies: Movie [] = [];
 
-  API_ROOT = "https://api.themoviedb.org/3/movie/now_playing?api_key=1dbbbb1e7496ae12052380ca2ebe5ae2";
+  API_ROOT = "https://api.themoviedb.org/3/movie/now_playing?language=it-it&api_key=1dbbbb1e7496ae12052380ca2ebe5ae2";
 
   constructor(private http: HttpClient) { 
     this.getMovies();
